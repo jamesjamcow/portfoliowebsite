8c1a677932e83bc5b092424618c326182e55ea1e
Jamerson <jamescao800@gmail.com>
2026-03-26 21:42:07 -0400

    got ride of the place holders from the init

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-    
+      <div
+  className="absolute inset-0 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] bg-size-[16px_16px]"
+></div><div></div>
     </>
   )
 }
